bee891d0bc38eddbd05460ebb62eb8205740bacf
Ebuube <onwutaebubegideon1555@gmail.com>
2023-12-05 12:58:56 +0000

    Created tests for comment

--- models/comment.py
+++ models/comment.py
@@ -8,6 +8,12 @@ from sqlalchemy.orm import relationship
 
 
 class Comment(BaseModel, Base):
+    """
+    Description of Comment's under a post
+    - content : content of the comment
+    - post_id : post it is referencing
+    - author_id : id of user who made the comment
+    """
     if models.storage_t == 'db':
         __tablename__ = "comments"
         content = Column(String(255), nullable=False)
@@ -19,11 +25,11 @@ class Comment(BaseModel, Base):
         created_at = Column(DateTime, default=datetime.utcnow)
     else:
         content = ""
-        post_id = 0
-        post = None
+        post_id = ""
         author_id = ""
-        author = None
-        created_at = None
+        # post = None # property method
+        # author = None # property method
+        # created_at = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
---
+++ tests/test_models/test_comment.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Tests for the Comment model
+"""
+from tests.test_models.test_base_model import test_BaseModel
+from models.comment import Comment
+
+
+class test_Comment(test_BaseModel):
+    """
+    Define extra tests for the ``Comment`` class
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization of the class' test
+        """
+        super().__init__(*args, **kwargs)
+
+    def test_comment_attrs(self):
+        """
+        Ensure that the correct attributes are present in the model
+        """
+        _cls = Comment
+
+        foo = _cls()
+        attrs = {
+                'content': str, 'author_id': str, 'post_id': str
+                }
+
+        for attr, attr_type in attrs.items():
+            with self.subTest(attr=attr, attr_type=attr_type):
+                self.assertTrue(hasattr(foo, attr))
+                self.assertEqual(type(getattr(foo, attr)), attr_type)
