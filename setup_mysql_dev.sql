ad28f07d6139788c61165419e0ad47b5eb327c5f
Ebuube <onwutaebubegideon1555@gmail.com>
2023-12-05 15:50:11 +0000

    Created set up script for database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Prepare a MySQL server for the development of the project
+-- Usage: cat 'setup_mysql_dev' | sudo mysql;
+CREATE DATABASE IF NOT EXISTS nervenex_dev_db;
+CREATE USER IF NOT EXISTS 'nervenex_dev'@'localhost' IDENTIFIED BY 'nervenex_dev_pwd';
+
+GRANT ALL PRIVILEGES ON nervenex_dev_db.* TO 'nervenex_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'nervenex_dev'@'localhost';
