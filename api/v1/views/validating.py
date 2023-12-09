81502ce3d1e581de371304e60ee387d7bfb445e1
arkoaikins <arkoaikins@gmail.com>
2023-12-09 15:38:16 -0500

    add e-mail validation

---
+++ api/v1/views/validating.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import validators
+
+def validate(req):
+    """ Validate Data """
+
+    if 'email' in req:
+        if not validators.email(req['email']):
+            return "Email is invalid."
+
+    return None  # Return None when no validation issues are found
