2e47db9e4c34f5f51be479c37219aae1b93391d5
arkoaikins <arkoaikins@gmail.com>
2023-12-09 21:08:23 -0500

    add routes for posts

---
+++ api/v1/views/posts.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+"""Objects that handle all default RestFul API actions for Posts"""
+from models.post import Post
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from flasgger.utils import swag_from
+
+@app_views.route('/posts', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/post/all_posts.yml')
+def get_posts():
+    """
+    Retrieves the list of all post objects
+    """
+    try:
+        all_posts = storage.all(Post).values()
+        list_posts = []
+        for post in all_posts:
+            list_posts.append(post.to_dict())
+
+        return jsonify(list_posts)
+    except Exception:
+        return make_response(jsonify({'message': 'Something is wrong'}), 400)
+
+
+@app_views.route('/posts/<post_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/post/get_post.yml', methods=['GET'])
+def get_post(post_id):
+    """Retrieves a specific post"""
+    try:
+        post = storage.get(Post, post_id).first()
+
+        if not post:
+            abort(404)
+
+        return jsonify(post.to_dict())
+    except Exception:
+        return make_response(jsonify({'message': 'Something is wrong'}), 400)
+    
+
+@app_views.route('/posts', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/post/post_post.yml', methods=['POST'])
+def create_post():
+    """Create a new post"""
+    try:
+        data = request.get_json()
+
+        if not data:
+            abort(400, description="Not a JSON")
+
+        if 'content' not in data:
+            abort(400, description="Missing content")
+        if 'thread_id' not in data:
+            abort(400, description="Missing thread_id")
+
+        if 'author_id' not in data:
+            abort(400, description="Missing author_id")
+
+        instance = Post(**data)
+        instance.save()
+        post = instance.to_dict()
+
+        return make_response(jsonify(post), 201)
+    except Exception:
+        return make_response(jsonify({'message': 'Something is wrong'}), 400)
+
+
+@app_views.route('/posts/<post_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/post/put_post.yml', methods=['PUT'])
+def update_post(post_id):
+    """Updates a post"""
+    try:
+        post = storage.get(Post, post_id).first()
+
+        if not post:
+            abort(404)
+
+        data = request.get_json()
+
+        if not data:
+            abort(400, description="Not a JSON")
+
+        ignore = ['id', 'created_at', 'updated_at']
+
+        for key, value in data.items():
+            if key not in ignore:
+                setattr(post, key, value)
+
+        storage.save()
+
+        return make_response(jsonify(post.to_dict()), 200)
+    except Exception:
+        return make_response(jsonify({'message': 'Something is wrong'}), 400)
+
+
+@app_views.route('/posts/<post_id>', methods=['DELETE'], strict_slashes=False)
+@swag_from('documentation/post/delete_post.yml', methods=['DELETE'])
+def delete_post(post_id):
+    """Deletes a post"""
+    try:
+        post = storage.get(Post, post_id).first()
+
+        if not post:
+            abort(404)
+
+        storage.delete(post)
+        storage.save()
+
+        return make_response(jsonify({'message': 'Post deleted successfully'}), 200)
+    except Exception:
+        return make_response(jsonify({'message': 'Something went wrong while deleting the post'}), 400)
+
