5d4c83943631df7102c23742526ca77493326660
Ebuube <onwutaebubegideon1555@gmail.com>
2023-12-14 01:35:26 +0000

    Implemented BASIC user authentication

--- api/v1/views/auth.py
+++ api/v1/views/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ objects that handle all default RestFul API actions for Users """
+from models.authorized import Authorized
 from models.user import User
 from models import storage
 from api.v1.views import app_views
@@ -8,6 +9,8 @@ from flask import abort, jsonify, make_response, request, session
 from hashlib import md5
 import validators
 
+
+# User session data structure for authentication
 
 @app_views.route('/login', methods=['POST'], strict_slashes=False)
 def login():
@@ -36,27 +39,32 @@ def login():
     for user in all_users:
         if user.email == data['email'] and\
                 user.password == hash_password:
+            Authorized.add(user)
             user_o = user.to_dict()
-            session['user'] = user_o
-            return (jsonify(user_o))
+            return make_response(jsonify(user_o), 201)
 
     return (make_response(
-        jsonify({'message': 'E-mail or password is incorrect'}), 400))
+        jsonify({'message': 'E-mail or password is incorrect'}), 401))
 
 
-@app_views.route('/logout', methods=['GET'], strict_slashes=False)
-def logout():
+@app_views.route('/logout/<user_id>', methods=['DELETE'], strict_slashes=False)
+def logout(user_id):
     """  User log out """
-    session.pop('user', None)
-    return (jsonify({'message': 'Logged out'}))
+    user = storage.get(User, user_id)
+    if Authorized.remove(user):
+        return make_response(
+                jsonify({'message': 'Successfully Logged out'}), 200)
+
+    return make_response(
+            jsonify({'message': 'No such user is logged in'}), 404)
 
 
-@app_views.route('/me', methods=['GET'], strict_slashes=False)
-def me():
+@app_views.route('/me/<user_id>', methods=['GET'], strict_slashes=False)
+def me(user_id):
     """ Get loged in User """
-    user = session.get('user')
-    if user is not None:
-        return (jsonify(user))
+    user = storage.get(User, user_id)
+    if Authorized.is_auth(user):
+        return make_response(jsonify(user.to_dict()), 200)
     return (make_response(jsonify({'message': 'User Not Found'}), 404))
 
 
@@ -87,6 +95,8 @@ def signup():
 
     new_user = User(**data)
     new_user.save()
+    # By default: a new user is automatically authorized
+    Authorized.add(new_user)
     user_data = new_user.to_dict()
 
-    return jsonify(user_data), 201
+    return make_response(jsonify(user_data), 201)
