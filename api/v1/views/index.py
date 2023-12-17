e45ed50d1dbba9ce4177da20f8a8524d38f7d05a
Ebuube <onwutaebubegideon1555@gmail.com>
2023-12-17 01:30:12 +0000

    Updated stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ Index """
+from models.answer import Answer
+from models.attempt import Attempt
+from models.authorized import Authorized
 from models.category import Category
+from models.comment import Comment
+from models.post import Post
 from models.question import Question
 from models.quiz import Quiz
+from models.resource import Resource
+from models.thread import Thread
 from models.user import User
 from models import storage
 from api.v1.views import app_views
@@ -18,7 +25,8 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
     """ Retrieves the number of each objects by type """
-    classes = [Category, Question, Quiz, User]
+    classes = [Answer, Attempt, Authorized, Category, Comment, Post,
+               Question, Quiz, Resource, Thread, User]
 
     num_objs = {}
     for cls in classes:
